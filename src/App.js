daa5ebcfeb0e4ab3a5acb947eae80bfd92766864
HenryDong <943082205@qq.com>
2019-08-01 18:42:58 +0800

    添加路由， 首页与详情

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import {GlobalStyle} from "./style";
 import store from './store'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import {Provider} from 'react-redux'
 import Header from './common/header'
+import Home from './pages/home'
+import Detail from './pages/detail'
 function App() {
   return (
     <Provider store={store}>
 		    <GlobalStyle />
 		    <Header />
+		    <Router>
+						    <Route exact path="/"  component={Home}/>
+						    <Route  path="/detail" component={Detail} />
+		    </Router>
     </Provider>
   );
 }
